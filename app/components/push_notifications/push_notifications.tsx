af0ab39ab6c19dfbd55fe259fe5c26360faba7e9
salvo <dafder45678@gmail.com>
2026-07-01 10:14:45 +0200

    fix: managed if user reject permission

--- app/components/push_notifications/push_notifications.tsx
+++ app/components/push_notifications/push_notifications.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import OneSignal from 'react-onesignal';
 
 export default function PushNotifications() {
+    const [hasUserDeniedPermission, setHasUserDeniedPermission] = useState<boolean>(false);
     useEffect(() => {
         const initOneSignal = async () => {
             // we avoid server error. This code should only be executed in browser
@@ -12,12 +13,26 @@ export default function PushNotifications() {
                     appId: process.env.NEXT_PUBLIC_ONE_SIGNAL_APP_ID ?? "",
                 })
 
-                // appear the notification panel
-                OneSignal.Slidedown.promptPush()
+                const hasPermission: boolean = OneSignal.Notifications.permission
+                if (!hasPermission) {
+                    setHasUserDeniedPermission(true)
+                } else {
+                    // appear the notification panel
+                    OneSignal.Slidedown.promptPush()
+                }
             }
         }
         initOneSignal()
     }, [])
+
+    if (hasUserDeniedPermission) {
+        return (
+            <div className="alert-permission">
+                <p>Le notifiche sono disattivate. Per ricevere i promemoria, devi attivarle dalle impostazioni del browser.</p>
+                <button onClick={() => OneSignal.Slidedown.promptPush()}>Attiva notifiche</button>
+            </div>
+        );
+    }
 
     return null
 }
